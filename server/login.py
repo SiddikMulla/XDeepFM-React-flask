fcc790077ab1d98e095ff99e323d8b1cb6f09d8f
sid@747 <sidmm747@gmail.com>
2024-04-29 21:23:49 +0530

    added py dictionaty for User[email,Pass,id]

---
+++ server/login.py
@@ -0,0 +1,11 @@
+
+# Dictionary containing user information (email, password, user_id)
+users = {
+    'user1@example.com': {'password': 'password', 'user_id': 'AHPAXGZTH4KAGQHOWQ4SGU4BN23Q'},
+    'user2@example.com': {'password': 'password', 'user_id': 'AGPH5EGCVPZPPTMO5OM5DGVYEMLA'},
+    'user3@example.com': {'password': 'password', 'user_id': 'AFRY27GPRRTESTN746JGSGNPGW5Q'},
+    'user4@example.com': {'password': 'password', 'user_id': 'AE3PLZHW6NXWBMZ76TDVFQG2MJFA'},
+    'user5@example.com': {'password': 'password', 'user_id': 'AFOHSZOUYV4P3STMGY3ZVWEQAFQQ'},
+    # Add more users as needed
+}
+
